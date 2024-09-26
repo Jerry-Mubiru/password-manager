9da16c9c7bb90b70e990c55821d80a65f4d10402
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2024-09-26 13:28:08 -0400

    Project creation

---
+++ Password Manager/src/Main.java
@@ -0,0 +1,19 @@
+// Create a program in Java.
+// The program will be a password manager.
+// Break into separate methods.
+// A password file is the basis for this.
+// If none exists, prompt the user to create one.
+// Create a key using PBKDF2. Store the salt along with a token.
+// A password file exists so ask the user for the password to access the file.
+// The same salt will be used for every password.
+
+// Methods off the bat.
+// PromptForLabel(), PromptForPasswordToAdd(), IsFileExistent()
+// When main is initiated, the first step to get the Global passcode.
+// CreateGlobalPasscode(), Encrypt(), Encode(), Decrypt(), Decode().
+// Hash() PBKDF2.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
